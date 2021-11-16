bf2e6ab7f18dcd8bf99ae0682726ab2ee39ff9a7
zhaokai <zhaokaimailer@163.com>
2021-11-16 22:37:11 +0800

    update

---
+++ src/components/Revenues.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="box-header">
+    <div id="left" :class="num===1?'active':''" @click="num=1">支出</div>
+    <div id="right" :class="num===2?'active':''" @click="num=2">收入</div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Revenues",
+  data(){
+    return{
+      num:1
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+
+.box-header {
+  display: flex;
+  justify-content:center;
+  text-align: center;
+  background-color: #e8e8e8;
+  padding-top: 18px;
+
+
+  div{
+    font-size:20px;
+    padding-bottom: 3px;
+  }
+  #left{
+    margin-right: 10px;
+  }
+}
+.active{
+  border-bottom: 2px solid black;
+}
+
+</style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,12 +1,27 @@
 <template>
   <Layout>
-    <p>Money.vue</p>
+    <Revenues/>
+    <div>
+      <div><Icon name="statistics"/>
+        <span>统计</span>
+      </div>
+
+      <tags/>
+    </div>
   </Layout>
 </template>
 
 <script lang="ts">
-  export default {
+import Revenues from "@/components/Revenues.vue";
+import Tags from '@/components/Tags.vue';
+export default {
     name: 'Money',
+  components:{
+      Revenues,
+      Tags
+    }
   };
 </script>
+<style lang="scss" scoped>
 
+</style>
